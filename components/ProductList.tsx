dcaaa23386f2da7228810527cffd56ba2d76a049
suhandani <suhandanisiddharta@gmail.com>
2023-12-15 13:27:34 +0700

    fix tailwind table

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -8,17 +8,17 @@ interface Props {
 
 export default function ProductList({ product }: Props) {
   return (
-    <tr className="border-b hover:bg-gray-100 transition">
+    <tr className="border-b hover:bg-gray-100 transition text-center">
       <td className="py-2 px-4">
         <img className="object-fill h-24 w-60" src={product.thumbnail} />
       </td>
-      <td className="py-2 px-4">{product.title}</td>
+      <td className="py-2 px-4 h-24 w-60">{product.title}</td>
       <td className="py-2 px-4">${product.price}</td>
       <td className="py-2 px-4">{product.discountPercentage}%</td>
       <td className="py-2 px-4">{product.rating}</td>
       <td className="py-2 px-4">{product.stock}</td>
-      <td className="py-2 px-4">{product.brand}</td>
-      <td className="py-2 px-4 capitalize">
+      <td className="py-2 px-4 h-24 w-60">{product.brand}</td>
+      <td className="py-2 px-4 capitalize h-24 w-60">
         <Link
           href={{
             pathname: "/category/",
